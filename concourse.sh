8c7f410f31b1d8ff415cdc99d008667d8baeebd5
Patrick Christopher <coffeepac@gmail.com>
2017-02-21 15:29:10 -0800

    add a testery script

---
+++ concourse.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -xe
+
+#  this is an experiment in using concourse CI for building K2
+
+cwd=`pwd`
+
+#  generate a config file
+${cwd}/k2.sh --generate
+
+#  set a cluster name
+sed -i 's/cluster: /cluster: concourse/g' ~/.kraken/config.yaml
+
+#  GOGO GADGET
+${cwd}/k2.sh up
+
+#  DOWN DOWN GADGET
+${cwd}/k2.sh down
