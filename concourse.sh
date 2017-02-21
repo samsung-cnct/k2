fd15624e722b772abeed3a82d16c3044215bab07
Patrick Christopher <coffeepac@gmail.com>
2017-02-21 15:40:21 -0800

    huh.  that was weird

--- concourse.sh
+++ concourse.sh
@@ -3,17 +3,16 @@
 set -xe
 
 #  this is an experiment in using concourse CI for building K2
-
-k2Dir=`pwd`\k2
+cd k2
 
 #  generate a config file
-${k2Dir}/up.sh --generate
+./up.sh --generate
 
 #  set a cluster name
 sed -i 's/cluster: /cluster: concourse/g' ~/.kraken/config.yaml
 
 #  GOGO GADGET
-${k2Dir}/up.sh
+./up.sh
 
 #  DOWN DOWN GADGET
-${k2Dir}/down.sh
+./down.sh
