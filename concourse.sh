b8a516d991ff9976b157779e0388d4b5e4f876ce
Patrick Christopher <coffeepac@gmail.com>
2017-02-21 15:38:46 -0800

    cwd issues

--- concourse.sh
+++ concourse.sh
@@ -4,16 +4,16 @@ set -xe
 
 #  this is an experiment in using concourse CI for building K2
 
-cwd=`pwd`
+k2Dir=`pwd`\k2
 
 #  generate a config file
-${cwd}/k2/up.sh --generate
+${k2Dir}/up.sh --generate
 
 #  set a cluster name
 sed -i 's/cluster: /cluster: concourse/g' ~/.kraken/config.yaml
 
 #  GOGO GADGET
-${cwd}/k2/up.sh
+${k2Dir}/up.sh
 
 #  DOWN DOWN GADGET
-${cwd}/k2/down.sh
+${k2Dir}/down.sh
