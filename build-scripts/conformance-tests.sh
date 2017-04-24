d4e3655981a9935d48d430d15f19e2da83eb53c4
Patrick Christopher <coffeepac@gmail.com>
2017-04-23 21:17:24 -0700

    okay, now run the e2e tests

---
+++ build-scripts/conformance-tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+set -x
+
+#  prep the local container with the test files
+#  fetch the test files
+KUBERNETES_RELEASE_VERSION=$1
+PWD=`pwd`
+platform=linux
+arch=amd64
+cache_dir="${PWD}/${KUBERNETES_RELEASE_VERSION}"
+mkdir -p "${cache_dir}"
+pushd "${cache_dir}"
+gsutil -mq cp "gs://kubernetes-release/release/${KUBERNETES_RELEASE_VERSION}/kubernetes.tar.gz" .
+gsutil -mq cp "gs://kubernetes-release/release/${KUBERNETES_RELEASE_VERSION}/kubernetes-test.tar.gz" .
+gsutil -mq cp "gs://kubernetes-release/release/${KUBERNETES_RELEASE_VERSION}/kubernetes-client-${platform}-${arch}.tar.gz" .
+popd
+
+#  unpack the test files
+target_dir="${PWD}/kube_tests_dir"
+mkdir -p "${target_dir}"
+tar --strip-components 1 -C "${target_dir}" -xzf "${cache_dir}/kubernetes.tar.gz"
+tar --strip-components 1 -C "${target_dir}" -xzf "${cache_dir}/kubernetes-test.tar.gz"
+tar --strip-components 3 -C "${target_dir}/platforms/${platform}/${arch}" -xzf "${cache_dir}/kubernetes-client-${platform}-${arch}.tar.gz"
+
+# setup output dir
+OUTPUT_DIR="${PWD}/output"
+mkdir -p "${OUTPUT_DIR}/artifacts"
+
+# setup gopath
+#export GOPATH="${WORKSPACE}/go"
+#mkdir -p "${GOPATH}"
+
+## run
+K2_CLUSTER_NAME=`echo $2 | tr -cd '[[:alnum:]]-'`
+export KUBE_CONFORMANCE_KUBECONFIG=${PWD}/cluster/aws/${K2_CLUSTER_NAME}/admin.kubeconfig
+export KUBE_CONFORMANCE_OUTPUT_DIR=${OUTPUT_DIR}/artifacts
+
+# TODO: unclear what part of k8s scripts require USER to be set
+KUBERNETES_PROVIDER=aws USER=jenkins ./hack/parallel-conformance.sh ${target_dir} | tee ${OUTPUT_DIR}/build-log.txt
+# tee isn't exiting >0 as expected, so use the exit status of the script directly
+conformance_result=${PIPESTATUS[0]}
+
+exit ${conformance_result}
