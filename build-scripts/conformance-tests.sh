ed056bdefe6427ce3515fc8bfb9b5042d6a607e4
Patrick Christopher <coffeepac@gmail.com>
2017-04-24 12:05:05 -0700

    full path for hack/e2e

--- build-scripts/conformance-tests.sh
+++ build-scripts/conformance-tests.sh
@@ -36,7 +36,7 @@ export KUBE_CONFORMANCE_KUBECONFIG=${PWD}/cluster/aws/${K2_CLUSTER_NAME}/admin.k
 export KUBE_CONFORMANCE_OUTPUT_DIR=${OUTPUT_DIR}/artifacts
 
 # TODO: unclear what part of k8s scripts require USER to be set
-KUBERNETES_PROVIDER=aws USER=jenkins ./hack/parallel-conformance.sh ${target_dir} | tee ${OUTPUT_DIR}/build-log.txt
+KUBERNETES_PROVIDER=aws USER=jenkins $PWD/hack/parallel-conformance.sh ${target_dir} | tee ${OUTPUT_DIR}/build-log.txt
 # tee isn't exiting >0 as expected, so use the exit status of the script directly
 conformance_result=${PIPESTATUS[0]}
 
