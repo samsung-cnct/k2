b7361da48d4b098aa10718a3ee6fa2a830d0e87b
Patrick Christopher <coffeepac@gmail.com>
2017-04-25 21:53:27 -0700

    pathing mk3

--- build-scripts/conformance-tests.sh
+++ build-scripts/conformance-tests.sh
@@ -18,7 +18,7 @@ mkdir -p "${target_dir}"
 tar -C "${target_dir}" -xzf "${cache_dir}/kubernetes.tar.gz"
 tar -C "${target_dir}" -xzf "${cache_dir}/kubernetes-test.tar.gz"
 tar -C "${cache_dir}/client" -xzf "${cache_dir}/kubernetes-client-${platform}-${arch}.tar.gz"
-mv "${cache_dir}/client/kubernetes/client/bin/kubectl" "${target_dir}/kubernetes/platforms/${platform}/${arch}"
+mv "${cache_dir}/kubernetes/client/bin/kubectl" "${target_dir}/kubernetes/platforms/${platform}/${arch}"
 
 # setup output dir
 OUTPUT_DIR="${PWD}/output"
