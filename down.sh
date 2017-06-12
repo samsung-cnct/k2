f1fee2fc6287e2e90856e4ab765f4719557bea27
Leah Petersen <leahnpetersen@gmail.com>
2017-06-12 09:07:32 -0400

    revisions to check for k2 failures and trigger crash app

--- down.sh
+++ down.sh
@@ -11,4 +11,21 @@ set -o pipefail
 my_dir=$(dirname "${BASH_SOURCE}")
 source "${my_dir}/lib/common.sh"
 
-ansible-playbook ${K2_VERBOSE} -i ansible/inventory/localhost ansible/down.yaml --extra-vars "${KRAKEN_EXTRA_VARS}kraken_action=down" --tags "${KRAKEN_TAGS}"
+# file to capture logs for crash app
+log_file=$"/k2-crash-application/logs"
+
+# check if ansible playbook returned error, if so, send to crash app
+function crash_test {
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		show_post_cluster_error
+		go run /k2-crash-application/crash-app.go $log_file
+	else
+		show_post_cluster
+	fi
+}
+
+# exit trap for crash app
+trap crash_test EXIT
+
+ansible-playbook ${K2_VERBOSE} -i ansible/inventory/localhost ansible/down.yaml --extra-vars "${KRAKEN_EXTRA_VARS}kraken_action=down" --tags "${KRAKEN_TAGS}" | tee $log_file
--- up.sh
+++ up.sh
@@ -14,6 +14,22 @@ source "${my_dir}/lib/common.sh"
 # setup a sigint trap
 trap control_c SIGINT
 
-DISPLAY_SKIPPED_HOSTS=0 ansible-playbook ${K2_VERBOSE} -i ansible/inventory/localhost ansible/up.yaml --extra-vars "${KRAKEN_EXTRA_VARS}kraken_action=up" --tags "${KRAKEN_TAGS}" || show_post_cluster_error
+# capture logs for crash app
+log_file=$"/k2-crash-application/logs"
 
-show_post_cluster
+# check if ansible return failure
+# if failure, send to crash app
+function crash_test {
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+		show_post_cluster_error
+		go run /k2-crash-application/crash-app.go $log_file
+	else
+		show_post_cluster
+	fi
+}
+
+# exit trap for crash app
+trap crash_test EXIT
+
+DISPLAY_SKIPPED_HOSTS=0 ansible-playbook ${K2_VERBOSE} -i ansible/inventory/localhost ansible/up.yaml --extra-vars "${KRAKEN_EXTRA_VARS}kraken_action=up" --tags "${KRAKEN_TAGS}" | tee $log_file
