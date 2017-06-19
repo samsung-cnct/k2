f0dbedfa5d4f8eb0758bfdb8c2a633a56a0f0c65
Kwangyoung Allen Kim <blackdog0403@gmail.com>
2017-06-19 14:31:16 -0700

    Add clusterstatus.sh to check cluster status

--- up.sh
+++ up.sh
@@ -10,25 +10,8 @@ set -o pipefail
 # pull in utils
 my_dir=$(dirname "${BASH_SOURCE}")
 source "${my_dir}/lib/common.sh"
-
-#for single cluster. Need choose logic for extendability for Multiple cluster
-CLUSTER_NAME=$(grep -A1 'clusters:' ${KRAKEN_CONFIG} | tail -n1 | awk '{print $3}')
-# echo $KRAKEN_CONFIG
-# echo $CLUSTER_NAME
-CLUSTER_NODE_COUNT=$(kubectl --kubeconfig=${KRAKEN_BASE}/${CLUSTER_NAME}/admin.kubeconfig get nodes | wc -l)
-# echo $CLUSTER_NODE_COUNT
-
-if [ ${CLUSTER_NODE_COUNT} -gt 2 ]; then
-  read -r -p "[ $CLUSTER_NAME ] cluster has been already spinned up. Do you want to continue to update cluster? [ y/N ]:" response
-  response=${response,,}    # tolower
-  if [[ "$response" =~ ^(yes|y)$ ]]; then
-    warn "Start spinning up cluster."
-  else
-    warn "Spinning up cluster has been canceled."
-   exit 0
-  fi
-fi
-
+my_dir=$(dirname "${BASH_SOURCE}")
+source "${my_dir}/lib/clusterstatus.sh"
 
 # setup a sigint trap
 trap control_c SIGINT
