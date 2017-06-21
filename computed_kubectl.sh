cde6f6fc58668392490832cb0c436d4a12ffc8c6
Nathan Zook <nathanzsamsung@gmail.com>
2017-06-21 12:58:46 -0700

    Security fix & clear breaking debug code

--- computed_kubectl.sh
+++ computed_kubectl.sh
@@ -8,10 +8,12 @@ set -o nounset
 set -o pipefail
 
 my_dir=$(dirname "${BASH_SOURCE}")
-VERSIONFILE=/tmp/$$.maxver
+VERSIONFILE=$(mktemp /tmp/$0.XXXXXX)
+rm ${VERSIONFILE}
+trap '{ rm -f -- "${VERSIONFILE}"; }' INT TERM HUP EXIT
 # Call separate script to hide our args from lib/common.sh
 ${my_dir}/max_k8s_version.sh ${VERSIONFILE} -c $1
 shift
-
-/opt/cnct/kubernetes/`cat ${VERSIONFILE} | cut -d . -f 1-2`/bin/kubectl $@
+K8S_VERSION=$(cat ${VERSIONFILE} | cut -d . -f 1-2)
 rm ${VERSIONFILE}
+/opt/cnct/kubernetes/${K8S_VERSION}/bin/kubectl $@
