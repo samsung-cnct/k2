fd9573e45f91123496edc3d3758158b6cd328cce
Patrick Christopher <coffeepac@gmail.com>
2017-07-11 23:28:25 -0700

    check that the variable is apssed in at all

--- max_k8s_version.sh
+++ max_k8s_version.sh
@@ -11,10 +11,12 @@ set -o pipefail
 my_dir=$(dirname "${BASH_SOURCE}")
 
 OUTFILE=$1
-
+if [ -z ${OUTFILE} ]; then 
+  echo "must specify a file to save the max version too"
+  exit 1
 if [ ! -f $OUTFILE ]; then
   echo "first arg must be a pre-created file.  should be created with mktemp"
-  exit 1
+  exit 2
 fi
 
 shift
