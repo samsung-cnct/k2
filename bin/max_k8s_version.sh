fd9b4b23162873efadb21df5bef924477734fcbc
Patrick Christopher <coffeepac@gmail.com>
2017-07-31 22:54:23 -0700

    elif correction

--- bin/max_k8s_version.sh
+++ bin/max_k8s_version.sh
@@ -14,7 +14,7 @@ OUTFILE=$1
 if [ -z ${OUTFILE} ]; then 
   echo "must specify a file to save the max version too"
   exit 1
-if [ ! -f $OUTFILE ]; then
+elif [ ! -f $OUTFILE ]; then
   echo "first arg must be a pre-created file.  should be created with mktemp"
   exit 2
 fi
