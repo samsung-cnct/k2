55757b0893d9e6fc91a8352bec07c5c4490fe863
Leah Petersen <leahnpetersen@gmail.com>
2017-12-13 12:50:23 -0500

    added provider flag for GKE

--- hack/gen-test-config.sh
+++ hack/gen-test-config.sh
@@ -3,6 +3,8 @@
 # to run locally
 # export CI_PROJECT_DIR=$"<local_directory>"
 
-./bin/up.sh --generate --config $CI_PROJECT_DIR/cluster/$1/config.yaml
+PROVIDER=$( echo "$1" | tr -s '[:lower:]' '[:upper:]')
+
+./bin/up.sh --generate --provider $PROVIDER --config $CI_PROJECT_DIR/cluster/$1/config.yaml
 
 build-scripts/update-generated-config.sh $CI_PROJECT_DIR/cluster/$1/config.yaml krakenlib-$CI_PIPELINE_ID
